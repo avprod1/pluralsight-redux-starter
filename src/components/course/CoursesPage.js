c91d61ac9a449b2d594cc15b4f3f5f59f8c36bec
Davis <u276000@global.ual.com>
2018-05-01 20:31:58 -0500

    Connect Container step

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,8 +21,8 @@ class CoursesPage extends React.Component {
     this.setState({course: course});
   }
   onClickSave() {
-    alert(`${this.state.course.title}`);
-    // this.props.dispatch(courseActions.createCourse(this.state.course));
+    // alert(`${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
   courseRow (course, index) {
     return(<div key={index}>{course.title}</div>);
@@ -31,7 +31,7 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
-        {/*{this.props.courses.map(this.courseRow)}*/}
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input type="text"
            onChange={this.onTitleChange}
@@ -45,16 +45,16 @@ class CoursesPage extends React.Component {
     );
   }
 }
-// CoursesPage.PropTypes = {
-//     dispatch: PropTypes.func.isRequired,
-//     courses: PropTypes.array.isRequired
-// };
-//
-// function mapStateToProps (state, ownProps){
-//   return {
-//     courses: state.courses
-//   };
-// }
+CoursesPage.PropTypes = {
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+};
 
-// export default connect(mapStateToProps)(CoursesPage);
-export default CoursesPage;
+function mapStateToProps (state, ownProps){
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
+
